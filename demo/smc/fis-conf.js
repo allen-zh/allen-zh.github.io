602ba67a980c4cb3cd89793734037e76a259506f
Allen <stori.tk@gmail.com>
2014-12-16 23:47:59 +0800

    add pages

--- demo/smc/fis-conf.js
+++ demo/smc/fis-conf.js
@@ -1,17 +1,24 @@
 fis.config.set('roadmap.domain', '/demo/smc-1.0.0');
 
 fis.config.merge({
-    modules : {
-        parser : {
-            less : 'less'
+    modules: {
+        parser: {
+            less: 'less'
         },
-        optimizer : {
-            html : 'html-compress'
+        optimizer: {
+            html: 'html-compress'
         }
     },
-    roadmap : {
-        ext : {
-            less : 'css'
+    roadmap: {
+        ext: {
+            less: 'css'
+        }
+    },
+    deploy: {
+        smc: {
+            //from参数省略，表示从发布后的根目录开始上传
+            //发布到当前项目的上一级的output目录中
+            to: '../smc-1.0.0'
         }
     }
 });
--- demo/smc/js/page.js
+++ demo/smc/js/page.js
@@ -7,185 +7,200 @@
 
 //程序入口
 //define(function(require) {
-(function(global){
-	var p = global.parallax;// = require("parallax.js");
-	p.speed = 300;
+(function(global) {
+    var p = global.parallax; // = require("parallax.js");
+    p.speed = 500;
 
-	// fixed control element
-	var $control = $('.js_control'),
-		$points = $('.point', $control),
-		scrolltop = document.body.scrollTop,
-		POINT_CUR_CLS = 'current';
+    // fixed control element
+    var $control = $('.js_control'),
+        $points = $('.point', $control),
+        scrolltop = document.body.scrollTop,
+        POINT_CUR_CLS = 'current';
 
-	var pageArray = [];
+    var pageArray = [];
 
-	function init(){
-		initPage();
-		initControl();
-	}
+    function init() {
+        initPage();
+        initControl();
+    }
 
-	function initPage(){
-		p.onload = updatePoints;
-		
-		for(var i = 1;i <= 4;i++){
-			//var year = i + 1999;
-			var pid = "page" + i;
+    function initPage() {
+        p.onload = updatePoints;
 
-			p.add($("#" + pid));
-			p[pid].onload = pageLoad;
-			p[pid].preload = pagePreLoad;
-			p[pid].index = pageArray.push(pid) - 1;
-		}
-		p.page1.show();
-	}
+        for (var i = 1; i <= 4; i++) {
+            //var year = i + 1999;
+            var pid = "page" + i;
 
-	function pagePreLoad() {
-		var $page = $("#" + this.key);
-		showOrHideTools.call(this);
-		$page.addClass('page-loaded');	
-	}
-	
-	function pageLoad() {
-		showOrHideTools.call(this, 'load');
-	}
+            p.add($("#" + pid));
+            p[pid].onload = pageLoad;
+            p[pid].preload = pagePreLoad;
+            p[pid].index = pageArray.push(pid) - 1;
+        }
+        p.page1.show();
+    }
 
-	function showOrHideTools(type){
-		
-	}
+    function pagePreLoad() {
+        var $page = $("#" + this.key);
+        var $inner = $('.page-inner', $page);
 
-	function initControl() {
+        $inner.hide();
+        showOrHideTools.call(this);
+        $page.addClass('page-loaded');
+    }
 
-		bindMouseWheel(document, function(e){
-			MouseWheelHandler(e, function(delta){
-				if(delta > 0){
-					onUp();
-				}else{
-					onDown();
-				}
-			}, 20);
-		});
+    function pageLoad() {
+        var $page = $("#" + this.key);
+        var $inner = $('.page-inner', $page);
+        $inner.css({
+            'margin-top': '+=50px'
+        }).show().animate({
+            'margin-top': '-=50px'
+        }, 200, 'easeOutQuad');
 
-		bindSwipe(document, function(swipeLeft, swipeUp){
-			//console.log(left, up);
-			if(swipeUp){
-				onUp();
-			}else{
-				onDown();
-			}
-		});
 
-		//fixScroll();
+        showOrHideTools.call(this, 'load');
+    }
 
-		$(document).keydown(function(e) {
-			if (e.keyCode == 38 || e.keyCode == 37) {
-				// if($topAnchor.offset().top == $pageLast.offset().top) 
-				// 	isTrigger = true;
+    function showOrHideTools(type) {
 
-				// if(isTrigger){
-					onUp(p.current.key);
-					return false;
-				//}
-			} else if (e.keyCode == 40 || e.keyCode == 39) {
-				//if(p.current.key != 'pageLast'){
-					onDown(p.current.key);
-					return false;
-				//}
-			}
-		});
+    }
 
-		$points.click(function() {
-			var pageKey = $(this).data('page'),
-				page = p[pageKey],
-				index,
-				lastPage = p.current,
-				lastIndex = lastPage.index;
+    function initControl() {
 
-			if(!page) return;
-			index = page.index;
+        bindMouseWheel(document, function(e) {
+            MouseWheelHandler(e, function(delta) {
+                if (delta > 0) {
+                    onUp();
+                } else {
+                    onDown();
+                }
+            }, 20);
+        });
 
-			if (index === lastIndex) return false;
-			if (index > lastIndex) {
-				page && goPage(pageKey, 'bottom');
-			}
+        bindSwipe(document, function(swipeLeft, swipeUp) {
+            //console.log(left, up);
+            if (swipeUp) {
+                onUp();
+            } else {
+                onDown();
+            }
+        });
 
-			if (index < lastIndex) {
-				page && goPage(pageKey, 'top');
-			}
-			return false;
-		});
-	}
-	function updatePoints(points) {
+        //fixScroll();
 
-		$points.removeClass(POINT_CUR_CLS);
-		$points.each(function(index) {
-			if ($(this).data('page') === p.current.key) {
-				$(this).addClass(POINT_CUR_CLS);
-			}
-		});
-	}
+        $(document).keydown(function(e) {
+            if (e.keyCode == 38 || e.keyCode == 37) {
+                // if($topAnchor.offset().top == $pageLast.offset().top) 
+                // 	isTrigger = true;
 
-	function bindMouseWheel(element, Handler){
+                // if(isTrigger){
+                onUp(p.current.key);
+                return false;
+                //}
+            } else if (e.keyCode == 40 || e.keyCode == 39) {
+                //if(p.current.key != 'pageLast'){
+                onDown(p.current.key);
+                return false;
+                //}
+            }
+        });
 
-		if (element.addEventListener) {
-		    // IE9, Chrome, Safari, Opera
-		    element.addEventListener("mousewheel", Handler, false);
-		    // Firefox
-		    element.addEventListener("DOMMouseScroll", Handler, false);
-		}
-		// IE 6/7/8
-		else if (element.attachEvent) {
-			element.attachEvent("onmousewheel", Handler);
-		}else{
-			element.onmousewheel = Handler;
-		}
-	}
+        $points.click(function() {
+            var pageKey = $(this).data('page'),
+                page = p[pageKey],
+                index,
+                lastPage = p.current,
+                lastIndex = lastPage.index;
 
-	var MouseWheelTimer;
-	var isTrigger = true;
-	function MouseWheelHandler(e, fn, delay) {
-    	// cross-browser wheel delta
-    	// if not isTrigger, return
-    	if(! isTrigger) return;
+            if (!page) return;
+            index = page.index;
 
-	    var e = window.event || e; // old IE support
-	    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-	    
-	    if(MouseWheelTimer){
-	    	clearTimeout(MouseWheelTimer);
-	    }
-	    if(delay){
-	    	MouseWheelTimer = setTimeout(function(){
-	    		MouseWheelTimer = 0;
-	    		fn(delta);
-	    	}, delay);
-	    }else{
-	    	fn(delta);
-	    }
-	}
+            if (index === lastIndex) return false;
+            if (index > lastIndex) {
+                page && goPage(pageKey, 'bottom');
+            }
 
-	var touchX, touchY;
-	function bindSwipe(ele, Handler){
-		if (!ele.addEventListener) return;
+            if (index < lastIndex) {
+                page && goPage(pageKey, 'top');
+            }
+            return false;
+        });
+    }
 
-		ele.addEventListener("touchstart", function(event){
-			event.preventDefault();
-			if (! event.changedTouches.length) return;
-			var touch = event.changedTouches[0];
-			touchX = touch.pageX;
-			touchY = touch.pageY;
-		}, false);
+    function updatePoints(points) {
 
-		ele.addEventListener("touchend", function(event){
-			event.preventDefault();
-			//alert(event.changedTouches.length);
-			if (! event.changedTouches.length) return;
-			var touch = event.changedTouches[0];
+        $points.removeClass(POINT_CUR_CLS);
+        $points.each(function(index) {
+            if ($(this).data('page') === p.current.key) {
+                $(this).addClass(POINT_CUR_CLS);
+            }
+        });
+    }
 
-			Handler(touch.pageX - touchX > 0, touch.pageY - touchY > 0);
-		}, false);
-	}
+    function bindMouseWheel(element, Handler) {
 
-	/*function fixScroll(){
+        if (element.addEventListener) {
+            // IE9, Chrome, Safari, Opera
+            element.addEventListener("mousewheel", Handler, false);
+            // Firefox
+            element.addEventListener("DOMMouseScroll", Handler, false);
+        }
+        // IE 6/7/8
+        else if (element.attachEvent) {
+            element.attachEvent("onmousewheel", Handler);
+        } else {
+            element.onmousewheel = Handler;
+        }
+    }
+
+    var MouseWheelTimer;
+    var isTrigger = true;
+
+    function MouseWheelHandler(e, fn, delay) {
+        // cross-browser wheel delta
+        // if not isTrigger, return
+        if (!isTrigger) return;
+
+        var e = window.event || e; // old IE support
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+
+        if (MouseWheelTimer) {
+            clearTimeout(MouseWheelTimer);
+        }
+        if (delay) {
+            MouseWheelTimer = setTimeout(function() {
+                MouseWheelTimer = 0;
+                fn(delta);
+            }, delay);
+        } else {
+            fn(delta);
+        }
+    }
+
+    var touchX, touchY;
+
+    function bindSwipe(ele, Handler) {
+        if (!ele.addEventListener) return;
+
+        ele.addEventListener("touchstart", function(event) {
+            event.preventDefault();
+            if (!event.changedTouches.length) return;
+            var touch = event.changedTouches[0];
+            touchX = touch.pageX;
+            touchY = touch.pageY;
+        }, false);
+
+        ele.addEventListener("touchend", function(event) {
+            event.preventDefault();
+            //alert(event.changedTouches.length);
+            if (!event.changedTouches.length) return;
+            var touch = event.changedTouches[0];
+
+            Handler(touch.pageX - touchX > 0, touch.pageY - touchY > 0);
+        }, false);
+    }
+
+    /*function fixScroll(){
 		var timer, timer2;
 		$pageLast.scroll(function(){
 			clearTimeout(timer);
@@ -203,35 +218,35 @@
 		});
 	}*/
 
-	function goPage(pageKey, fnName) {
-		var page = p[pageKey];
+    function goPage(pageKey, fnName) {
+        var page = p[pageKey];
 
-		page[fnName] && page[fnName]();
-	}
+        page[fnName] && page[fnName]();
+    }
 
-	function onUp() {
-		var index = p.current.index;
-		index--;
+    function onUp() {
+        var index = p.current.index;
+        index--;
 
-		pageArray[index] && goPage(pageArray[index], 'top');
-		return false;
-	}
+        pageArray[index] && goPage(pageArray[index], 'top');
+        return false;
+    }
 
-	function onDown() {
-		var index = p.current.index;
-		index++;
-		pageArray[index] && goPage(pageArray[index], 'bottom');
-		return false;
-	}
+    function onDown() {
+        var index = p.current.index;
+        index++;
+        pageArray[index] && goPage(pageArray[index], 'bottom');
+        return false;
+    }
 
-	global.page = {
-		init 	: init, 
-		onDown 	: onDown,
-		onUp 	: onUp
-	}
-	// module.exports = {
-	// 	init 	: init, 
-	// 	onDown 	: onDown,
-	// 	onUp 	: onUp
-	// }
+    global.page = {
+            init: init,
+            onDown: onDown,
+            onUp: onUp
+        }
+        // module.exports = {
+        // 	init 	: init, 
+        // 	onDown 	: onDown,
+        // 	onUp 	: onUp
+        // }
 })(window);
