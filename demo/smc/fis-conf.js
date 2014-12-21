695c7e918ecfc50966b4d29bf4dde809ff56b01a
zhangwentao <zhangwentao@baidu.com>
2014-12-22 00:43:52 +0800

    release 1.0.1

--- demo/smc/fis-conf.js
+++ demo/smc/fis-conf.js
@@ -1,4 +1,4 @@
-fis.config.set('roadmap.domain', '/demo/smc-1.0.0');
+fis.config.set('roadmap.domain', '/demo/smc-1.0.1');
 
 fis.config.merge({
     modules: {
@@ -27,7 +27,7 @@ fis.config.merge({
         smc: {
             //from参数省略，表示从发布后的根目录开始上传
             //发布到当前项目的上一级的output目录中
-            to: '../smc-1.0.0'
+            to: '../smc-1.0.1'
         }
     }
 });
