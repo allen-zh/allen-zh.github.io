b43f152ce056e589226815c41e2d341c3e57399e
zhangwentao <zhangwentao@baidu.com>
2014-12-23 23:40:59 +0800

    release 1.0.4

--- demo/smc/fis-conf.js
+++ demo/smc/fis-conf.js
@@ -1,4 +1,4 @@
-fis.config.set('roadmap.domain', '/demo/smc-1.0.3');
+fis.config.set('roadmap.domain', '/demo/smc-1.0.4');
 
 fis.config.merge({
     modules: {
@@ -21,13 +21,19 @@ fis.config.merge({
     roadmap: {
         ext: {
             less: 'css'
-        }
+        },
+        path: [{
+            reg: '**.otf',
+            release: '/css/font$&',
+            useHash: true,
+            userDomain: true
+        }]
     },
     deploy: {
         smc: {
             //from参数省略，表示从发布后的根目录开始上传
             //发布到当前项目的上一级的output目录中
-            to: '../smc-1.0.3'
+            to: '../smc-1.0.4'
         }
     }
 });
